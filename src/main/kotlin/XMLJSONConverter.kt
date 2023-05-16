d4c73d65d2b9024be659fa5463530d2aa8f29846
wilwarner6 <wwarner7@gatech.edu>
2023-05-16 17:59:49 -0400

    add jsonToXml logic

--- src/main/kotlin/XMLJSONConverter.kt
+++ src/main/kotlin/XMLJSONConverter.kt
@@ -4,5 +4,10 @@ import org.json.XML
 class XMLJSONConverter {
     fun convertXmlToJson(xml: String) {
         val json = XML.toJSONObject(xml)
+        return json.toString()
+    }
+    fun jsonToXml(json: String): String {
+        val jsonObject = JSONObject(json)
+        return XML.toString(jsonObject)
     }
 }
